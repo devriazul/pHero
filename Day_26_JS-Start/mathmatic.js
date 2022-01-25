dd0fbc93f96c8a3d7adc86f4bb2d38c23295cb7a
devriazul <engr.riazul@gmail.com>
2022-01-25 21:09:16 +0600

    day 26

--- Day_26_JS-Start/mathmatic.js
+++ Day_26_JS-Start/mathmatic.js
@@ -1,6 +1,9 @@
 var onionPrice = 43;
 var eggPrice = 42;
 
+var total = onionPrice+eggPrice;
+
 console.log(onionPrice);
 console.log(eggPrice);
 console.log(onionPrice+eggPrice);
+console.log(total);
