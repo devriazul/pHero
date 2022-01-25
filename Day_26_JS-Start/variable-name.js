2c5ef1a3d62ebe2d79f352c0b9faec0666bc7105
devriazul <engr.riazul@gmail.com>
2022-01-25 21:01:52 +0600

    day 26

--- Day_26_JS-Start/variable-name.js
+++ Day_26_JS-Start/variable-name.js
@@ -9,3 +9,16 @@ var first name = "Riaz";
 // quote is not allowed
 
 var "first name" = "riaz";
+
+var my-office-address = "mirpur";
+var my_office_address = "mirpur";
+var myfficeaddress = "mirpur";
+
+var myOfficeAddress = "mirpur";
+
+
+var PIE = 3.14;
+
+var 99price = 99;
+
+var p99 = 99;
