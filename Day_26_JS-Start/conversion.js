a8bea36f5123ad29e4508d96ee0e346816dbe9c5
devriazul <engr.riazul@gmail.com>
2022-01-25 23:24:22 +0600

    day 26

--- Day_26_JS-Start/conversion.js
+++ Day_26_JS-Start/conversion.js
@@ -4,3 +4,11 @@ var lName = "Islam";
 var fullName = fName + ' ' + lName;
 
 console.log(fullName);
+
+var age = "28.3";
+
+var age1 = parseFloat(age);
+var age2 = parseInt(age);
+
+console.log(age1);
+console.log(age2);
