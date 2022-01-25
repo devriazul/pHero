2cd1c8384944081cda511a0302ad08d4e38948cf
devriazul <engr.riazul@gmail.com>
2022-01-25 23:50:30 +0600

    day 26

--- Day_26_JS-Start/variabletype.js
+++ Day_26_JS-Start/variabletype.js
@@ -1,8 +1,10 @@
 var name = "riaz";
 var age = 28;
+var cgpa = 3.21;
 var unmarried = true;
 
 
 console.log(typeof name);
 console.log(typeof age);
+console.log(typeof cgpa);
 console.log(typeof unmarried);
