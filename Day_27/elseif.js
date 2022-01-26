d0a8981ad61c3164dfb4855874498f338c9aa797
devriazul <engr.riazul@gmail.com>
2022-01-26 21:33:06 +0600

    Day 27

---
+++ Day_27/elseif.js
@@ -0,0 +1,18 @@
+var gotJob = true;
+var moneySaved = 500000;
+var hasFlat = false;
+
+
+
+// if (gotJob == true && moneySaved > 30000) {
+//     console.log('Cholo Bia kore feli');
+// }else{
+//     console.log('Tor kopale bia nai');
+// }
+
+
+if (gotJob == true && moneySaved > 30000 && hasFlat == true) {
+    console.log('Cholo Bia kore feli');
+}else{
+    console.log('Tor kopale bia nai');
+}
