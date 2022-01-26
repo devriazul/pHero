38feb2b0ee0cdfc8c36f16d71bf0d6ef30cea572
devriazul <engr.riazul@gmail.com>
2022-01-26 21:48:55 +0600

    Day 27

--- Day_27/nested-condition.js
+++ Day_27/nested-condition.js
@@ -1,14 +1,26 @@
 var danishPrice = 50;
 var butterBread = 25;
 var toastBiscuit = 10;
-var myBudget = 9;
+var myBudget = 90;
+var packedWell = false;
 
-if (danishPrice < myBudget) {
-    console.log('Danish Khabo');
-}else if (butterBread < myBudget){
-    console.log('butter bon khamu');
-}else if (toastBiscuit < myBudget){
-    console.log ('Toast Khamu');
+// if (danishPrice < myBudget) {
+//     console.log('Danish Khabo');
+// }else if (butterBread < myBudget){
+//     console.log('butter bon khamu');
+// }else if (toastBiscuit < myBudget){
+//     console.log ('Toast Khamu');
+// }else{
+//     console.log('batasha khamu');
+// }
+
+
+if (danishPrice < myBudget){
+    if (packedWell == true){
+        console.log('Danish Khabo');
+    }else{
+        console.log('Machi Khaya felce');
+    }
 }else{
-    console.log('batasha khamu');
+    console.log('Taka nai vai');
 }
