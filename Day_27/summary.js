d29a8128981c76c4556efe3e1dee19e007dec784
devriazul <engr.riazul@gmail.com>
2022-01-26 22:03:30 +0600

    Update

--- Day_27/summary.js
+++ Day_27/summary.js
@@ -2,3 +2,12 @@ var tableLenght = 12;
 var tourDestination = ['dhaka', 'chittagong'];
 
 tourDestination.indexOf('dhaka');
+
+var secondDestination = tourDestination[1];
+
+tourDestination[1] = ('sylhet');
+
+
+tourDestination.push('london');
+tourDestination.pop();
+
