6a48b98e5cb59158e31a6bfba1130e6cc42d5308
devriazul <engr.riazul@gmail.com>
2022-01-26 20:44:58 +0600

    Day 27 Started

--- Day_27/array_index.js
+++ Day_27/array_index.js
@@ -2,4 +2,10 @@ var bookPages = ['hablu', 'bolod', 'choddogusti', 'battery', 'paracetamol', 'gym
 
 var batteryIndex = bookPages.indexOf('battery');
 
-console.log(batteryIndex);
+// console.log(batteryIndex);
+
+console.log(bookPages);
+
+bookPages[1] = 'chalak';
+
+console.log(bookPages);
