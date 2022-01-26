705843c0840c351a9b54d42b3fa414706a3704b9
devriazul <engr.riazul@gmail.com>
2022-01-26 20:51:10 +0600

    Day 27

--- Day_27/push-pop.js
+++ Day_27/push-pop.js
@@ -1,7 +1,13 @@
 //add element by push and remove by pop
 
 var lastBench = ['kalam', 'salam', 'balam'];
+
+console.log(lastBench);
+
 lastBench.push('riazul');
+
+console.log(lastBench);
+
 lastBench.pop('riazul');
 
 console.log(lastBench);
