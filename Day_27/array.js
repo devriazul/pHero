89e79331119900d637a0c06afe37081883695907
devriazul <engr.riazul@gmail.com>
2022-01-26 20:30:13 +0600

    Day 27 Started

--- Day_27/array.js
+++ Day_27/array.js
@@ -1,13 +1,16 @@
 var friendsAge = [11, 12, 13, 14, 15];
 
-console.log(friendsAge[0]);
+// console.log(friendsAge[0]);
 
 var naikas = ['katrina', 'deepika', 'priyanka'];
 
-console.log(naikas[1]);
+// console.log(naikas[1]);
 
 var oddNumbers = [1, 3, 5, 7];
-var vowels = [a, e, i, o, u];
+// var vowels = [a, e, i, o, u];
 
+
+console.log(naikas.length);
+console.log(naikas);
 
 
---
+++ Day_27/array_index.js
@@ -0,0 +1,5 @@
+var bookPages = ['hablu', 'bolod', 'choddogusti', 'battery', 'paracetamol', 'gymnasium'];
+
+var batteryIndex = bookPages.indexOf('battery');
+
+console.log(batteryIndex);
