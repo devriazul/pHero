04385cf2c335014bc82716f6cc43492910cb90f1
devriazul <engr.riazul@gmail.com>
2022-01-26 21:10:45 +0600

    Day 27

--- Day_27/comparison.js
+++ Day_27/comparison.js
@@ -1,4 +1,14 @@
-console.log(5 > 6);
-console.log(5 < 6);
-console.log(5 == 6);
-console.log(5 != 6);
+// console.log(5 > 6);
+// console.log(5 < 6);
+// console.log(5 == 6);
+// console.log(5 != 6);
+
+var myLove = 99;
+var herLove = 100;
+
+// console.log(myLove < herLove);
+// console.log(myLove == herLove);
+// console.log(myLove != herLove);
+
+console.log(studyGood && goodPerson); //and (must be 2 condition fullfilled)
+console.log(studyGood || goodPerson); //or (must be fullfilled any 1 condition)
