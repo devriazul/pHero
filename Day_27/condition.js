1d33e5ddfb5f278a3c1da372e498ddc1ea1d79bc
devriazul <engr.riazul@gmail.com>
2022-01-26 21:18:25 +0600

    Day 27

---
+++ Day_27/condition.js
@@ -0,0 +1,17 @@
+var isFoodReady = false;
+
+if (isFoodReady == true) {
+    console.log('Khabo');
+}else{
+    console.log('khabo na');
+}
+
+
+var iphonePrice = 70000;
+var myBudget = 10000;
+
+if (iphonePrice == myBudget) {
+    console.log('iphone nibo');
+}else{
+    console.log('ami groib');
+}
