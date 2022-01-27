8c92585b245db0df08aa40e3647f144e0ec32370
devriazul <engr.riazul@gmail.com>
2022-01-28 00:33:26 +0600

    Day 28

--- Day_28/for-loop.js
+++ Day_28/for-loop.js
@@ -1,4 +1,9 @@
-for (var roastGiven = 0; roastGiven < 7; roastGiven++) {
-    console.log("Roast den vai");
-    console.log(roastGiven);
+// for (var roastGiven = 0; roastGiven < 7; roastGiven++) {
+//     console.log("Roast den vai");
+//     console.log(roastGiven);
+// }
+
+for (var i = 1; i < 7; i++) {
+    console.log(i);
+    
 }
