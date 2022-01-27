b9849672c9ed7d9308cbf0d017c81d20f1f4fbbe
devriazul <engr.riazul@gmail.com>
2022-01-27 21:46:14 +0600

    Name

--- Day_28/conceptual.js
+++ Day_28/conceptual.js
@@ -3,3 +3,12 @@ var myName = "Riazul Islam's";
 var line1 = "Hello i am "+ myName;
 
 console.log(line1.indexOf(0));
+
+
+var student1 = {
+    fName: "Riazul",
+    lName: "Islam",
+    location: "Dhaka"
+}
+
+console.log(student1.location);
