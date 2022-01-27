73cdb87d957351c5d5f50d73e85fc9a24a95fecc
devriazul <engr.riazul@gmail.com>
2022-01-28 00:43:01 +0600

    Day 28

---
+++ Day_28/array-loop.js
@@ -0,0 +1,6 @@
+var numbers = [ 1, 3 , 5, 7];
+
+for (var i = 0; i < 4; i++) {
+    var element = numbers[i];
+    console.log(element);
+}
---
+++ Day_28/summary.js
@@ -0,0 +1,12 @@
+// Basic while loop 
+var i =0;
+
+while (i <= 7) {
+    console.log(i);
+    i++;
+}
+
+// basic for loop 
+for (var i = 0; i <= 7; i++) {
+    console.log(i);    
+}
