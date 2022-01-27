8b7eb70e55c74cea5bafc5a4a8b3c357ad0031b8
devriazul <engr.riazul@gmail.com>
2022-01-28 00:20:21 +0600

    Day 28

---
+++ Day_28/odd-numbers.js
@@ -0,0 +1,22 @@
+var number = 1;
+
+// while (number <= 20) {
+//     console.log(number);
+//     number++;
+// }
+
+// odd number 
+
+while (number <=20) {
+    console.log(number);
+    number = number+2;
+}
+
+// even number 
+
+var number = 0;
+
+while (number <=20) {
+    console.log(number);
+    number = number+2;
+}
