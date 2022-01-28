8ac91966065867081efea540434d24f61fe3fc06
devriazul <engr.riazul@gmail.com>
2022-01-28 21:01:34 +0600

    Day-29

--- Day_29/multiple-parameter.js
+++ Day_29/multiple-parameter.js
@@ -1,7 +1,10 @@
 function addTwoNumbers(number1,number2){
     console.log(number1, number2);
+    var total = number1 + number2;
+    return total;
 }
 
 var fisrtNumber = 35;
 var secondNumber = 47;
-addTwoNumbers(fisrtNumber,secondNumber);
+var result = addTwoNumbers(fisrtNumber,secondNumber);
+console.log('result value', result);
