d03016eb2f8cad48b52148883413ca17598b8704
devriazul <engr.riazul@gmail.com>
2022-01-28 21:49:12 +0600

    Day-29

--- Day_29/object-propeties.js
+++ Day_29/object-propeties.js
@@ -1,9 +1,12 @@
 var computer = {
     color: "silver",
     price: 28000,
-    storage: "156GB",
+    storage: "256GB",
     processor: "i5"
 };
+
+var storageProperty = "storage";
+computer['storage'] =512;
 computer.price = 20000;
 computer["price"] = 19000; //alternative way of declare object property for change
 var computerPrice = computer.price;
