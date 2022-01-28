207b30a019aa7df388b6805aaf986af2216a82d9
devriazul <engr.riazul@gmail.com>
2022-01-28 22:11:21 +0600

    Day-29

--- Day_29/break-continue.js
+++ Day_29/break-continue.js
@@ -1,8 +1,15 @@
-var i =0;
-while (i<15) {
+// var i =0;
+// while (i<15) {
+//     console.log(i);
+//     if (i == 5) {
+//         break;
+//     }
+//     i++;
+// }
+
+for (var i = 0; i < 20; i++){
     console.log(i);
-    if (i == 5) {
+    if (i==7) {
         break;
     }
-    i++;
 }
