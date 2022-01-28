acfad5b449f5df35c9eaf6c1d9ffe29719b4fd31
devriazul <engr.riazul@gmail.com>
2022-01-28 20:24:16 +0600

    Day-29

--- Day_29/recap.js
+++ Day_29/recap.js
@@ -1,4 +1,8 @@
+// 1. Variable 
+
 var favouriteBook = '4 Hour Work Week';
+
+// 2.array 
 var bookList = ['love story', 'dead story', 'twilight', 'hooked', 'start with why', 'shoe dog'];
 
 var indexFind = bookList.indexOf('shoe dog');
@@ -6,3 +10,27 @@ bookList[3] = 'story brand';
 bookList.push('small giant');
 bookList.pop();
 
+// 3.condition 
+
+if (bookList[1] == 'hooked'){
+    console.log('i am hooked');
+}else
+    console.log('I am not hooked');
+
+// 4.Loop 
+
+// While Loop
+
+var i =0;
+while(i < 15){
+    console.log(i);
+    console.log('loop');
+    i++;
+}
+
+//for loop
+
+for (var i = 0; i < 15; i++) {
+    console.log(i);
+    
+}
