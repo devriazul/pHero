638f288f94dbbfce4a19fdf21601073785bad2b0
devriazul <engr.riazul@gmail.com>
2022-01-28 20:51:05 +0600

    Day-29

--- Day_29/parameter.js
+++ Day_29/parameter.js
@@ -1,7 +1,11 @@
 function bringSingara(taka){
-    console.log('Singara Price', taka);
+    console.log('Singara Jonno Diche', taka);
     console.log('Mama singara den');
+    var singaraPrice =10;
+    var singaraQuantity = taka / singaraPrice;
+    return singaraQuantity;
 }
 
 var money = 250;
-bringSingara(money);
+var singara = bringSingara(money);
+console.log('Ai nen singara', singara);
