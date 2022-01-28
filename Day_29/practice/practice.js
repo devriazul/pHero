1dc2fbf68297d2c48335f3bbee929f999a9be5d2
devriazul <engr.riazul@gmail.com>
2022-01-28 14:39:32 +0600

    Float & INT

--- Day_29/practice/practice.js
+++ Day_29/practice/practice.js
@@ -13,3 +13,11 @@ var numbers = "10";
 
 console.log(numbers);
 console.log (parseFloat(numbers));
+
+var age = "28.6";
+
+var age1 = parseFloat(age);
+var age2 = parseInt(age);
+
+console.log(age1);
+console.log(age2);
