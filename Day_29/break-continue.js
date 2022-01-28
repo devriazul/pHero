f35f577f5459a56254fa319690e8656515ffb407
devriazul <engr.riazul@gmail.com>
2022-01-28 22:16:21 +0600

    Day-29

--- Day_29/break-continue.js
+++ Day_29/break-continue.js
@@ -7,9 +7,18 @@
 //     i++;
 // }
 
-for (var i = 0; i < 20; i++){
-    console.log(i);
-    if (i==7) {
+// for (var i = 0; i < 20; i++){
+//     console.log(i);
+//     if (i==7) {
+//         break;
+//     }
+// }
+
+var numbers = [1, 2, 3, 5, 4, 6, 7];
+for (var i = 0; i < numbers.length; i++) {
+    var number = numbers [i];
+    console.log(number);  
+    if(number > 5){
         break;
-    }
+    }  
 }
