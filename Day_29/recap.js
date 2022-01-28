39faedd8e57014bbe17238bf03acfb813e04ca26
devriazul <engr.riazul@gmail.com>
2022-01-28 20:16:00 +0600

    Day-29

--- Day_29/recap.js
+++ Day_29/recap.js
@@ -1,3 +1,8 @@
 var favouriteBook = '4 Hour Work Week';
 var bookList = ['love story', 'dead story', 'twilight', 'hooked', 'start with why', 'shoe dog'];
 
+var indexFind = bookList.indexOf('shoe dog');
+bookList[3] = 'story brand';
+bookList.push('small giant');
+bookList.pop();
+
