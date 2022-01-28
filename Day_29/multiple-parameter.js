44a325847be0da97360805cf2fab5abf980d6731
devriazul <engr.riazul@gmail.com>
2022-01-28 21:09:55 +0600

    Day-29

--- Day_29/multiple-parameter.js
+++ Day_29/multiple-parameter.js
@@ -8,3 +8,27 @@ var fisrtNumber = 35;
 var secondNumber = 47;
 var result = addTwoNumbers(fisrtNumber,secondNumber);
 console.log('result value', result);
+
+
+// function multiply two numbers 
+
+function multiplyTwoNumbers(num1,num2){
+    var result = num1 * num2;
+    return result;
+}
+
+var totalAmount = multiplyTwoNumbers(5,100);
+console.log('Total Taka:',totalAmount);
+
+// Substraction function (-)
+
+function substractionBetwenTwo(nums1,nums2){
+    var subsResult = nums1 - nums2;
+    return subsResult;
+}
+var totalAfterSubs = substractionBetwenTwo(1000,400);
+console.log('Total Figure after Substract:', totalAfterSubs);
+
+
+
+// Divide function (/)
