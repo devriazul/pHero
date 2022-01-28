50494abb2fd519330aa3c9ba1560ec8100591e02
devriazul <engr.riazul@gmail.com>
2022-01-28 14:24:41 +0600

    Practice

---
+++ Day_28/quiz.practice.js
@@ -0,0 +1,2 @@
+var name = "false";
+console.log(typeof name);
--- Day_28/summary.js
+++ Day_28/summary.js
@@ -1,12 +1,12 @@
 // Basic while loop 
-var i =0;
+// var i =0;
 
-while (i <= 7) {
-    console.log(i);
-    i++;
-}
+// while (i <= 7) {
+//     console.log(i);
+//     i++;
+// }
 
 // basic for loop 
-for (var i = 0; i <= 7; i++) {
+for (var i = 0; i < 10; i++) {
     console.log(i);    
 }
