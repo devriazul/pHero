978ad063f6a35f24b85a7d2c58d17b6d93b50a08
devriazul <engr.riazul@gmail.com>
2022-01-29 23:19:41 +0600

    Day 30

--- Day_30/main/leapyear.js
+++ Day_30/main/leapyear.js
@@ -9,3 +9,7 @@ function isLeapYear(year){
 const myYear = 2087;
 const isMyYearLeapYear = isLeapYear(myYear);
 console.log("Is my Year Leap Year", isMyYearLeapYear);
+
+const yourYear =2000;
+const isYourYearLeapYear = isLeapYear(yourYear);
+console.log("is your year leap year", isYourYearLeapYear);
