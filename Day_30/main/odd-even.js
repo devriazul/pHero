0c0b6d007855ad1849fcccd790b386adb6774243
devriazul <engr.riazul@gmail.com>
2022-01-29 22:52:13 +0600

    Day 30

--- Day_30/main/odd-even.js
+++ Day_30/main/odd-even.js
@@ -1,3 +1,18 @@
-const number = 5;
-var result = number % 2;
-console.log(result == 0 );
+// const number = 5;
+// var result = number % 2;
+// console.log(result == 0 );
+
+// const number = 145;
+
+
+function isEven(number){
+    if (number % 2 == 0){
+        return true;
+    }return false;
+}
+
+const myNumber = 1641;
+const isMyNumEven = isEven (myNumber);
+
+const herNumber =1992;
+const isHerNumEven = isEven(herNumber);
