ad2e0b9fb09d1e356cf97bada7f09401775c5d4b
devriazul <engr.riazul@gmail.com>
2022-01-29 23:46:52 +0600

    Day 30

---
+++ Day_30/main/factorial-function.js
@@ -0,0 +1,16 @@
+// let factorial = 1;
+// for (let i = 1; i <=5; i++) {
+//     factorial = factorial * i; 
+// }
+// console.log(factorial);
+
+function getFactorial(number){
+    let factorial = 1;
+    for (let i = 1; i <= number; i++) {
+        factorial = factorial * i;        
+    }
+    return factorial;
+}
+
+var firstFactorial = getFactorial(7);
+console.log('Factorial of 7 is: ', firstFactorial);
