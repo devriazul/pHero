27d896782b49a7627817553c7d69cc2947f26182
devriazul <engr.riazul@gmail.com>
2022-01-29 20:58:51 +0600

    Day 30

--- Day_30/main/recap.js
+++ Day_30/main/recap.js
@@ -1,2 +1,19 @@
-var time = new Date;
-console.log(time);
+var time = '10:15pm';
+var bookPrice = 150;
+var isWhite = false;
+
+// array
+
+var partners = ['selim', 'sharif', 'hridoy', 'riaz'];
+var elementsCount = partners.length;
+var riazIndex = partners.indexOf('riaz');
+partners.push ('bari');
+partners.pop();
+
+// conditional
+
+if (bookPrice < 120){
+    console.log("I will buy!");
+}else{
+    console.log(Kichu Discount den);
+}
