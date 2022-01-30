082f3a5b1a44b29fbe5417da34ffc1655bb49374
devriazul <engr.riazul@gmail.com>
2022-01-30 22:18:05 +0600

    Day 31

--- Day_31/conceptual.js
+++ Day_31/conceptual.js
@@ -1,4 +1,4 @@
-//make a calculator
+//--make a calculator
 
 // function sumOfNumbers (num1, num2){
 //     var result = num1 + num2;
@@ -7,9 +7,9 @@
 
 // console.log(sumOfNumbers(20,9));
 
-//Problem solving with function
+//--Problem solving with function
 
-//1. Write a simple function that takes name as parameter and then return a custom message.
+//--1. Write a simple function that takes name as parameter and then return a custom message.
 
 // function message(name){
 //     var welcomeMessage = "Hello " + name + " sir, Welcome to Fast IT Bangladesh";
@@ -19,4 +19,4 @@
 // var name = message ("Riazul");
 // console.log(name);
 
-// write a function for a food delivery service that sell "pizza" only. this function takes the food name & quantity as parameter and then return a custom message with order info. This function should be smart enought to handle wrong parameter inputs.
+//-- write a function for a food delivery service that sell "pizza" only. this function takes the food name & quantity as parameter and then return a custom message with order info. This function should be smart enought to handle wrong parameter inputs.
