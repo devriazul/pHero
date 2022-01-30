a6fde300479b7c1a61df308f08760c5feddd9907
devriazul <engr.riazul@gmail.com>
2022-01-30 23:44:59 +0600

    Day 31

--- Day_31/conceptual.js
+++ Day_31/conceptual.js
@@ -1,4 +1,9 @@
-for (let i = 1; i < 11; i++) {
-    console.log(i);
-    
+function printer(numberLimit){
+    for (let i = 1; i < numberLimit + 1; i++) {
+        console.log(i);
+    }
 }
+
+printer (10);
+printer (15);
+printer (20);
