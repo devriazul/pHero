d080d41bb8679644caa36a87430c3d5fb9ae5c0f
devriazul <engr.riazul@gmail.com>
2022-01-30 22:34:37 +0600

    Day 31

--- Day_31/conceptual.js
+++ Day_31/conceptual.js
@@ -1,11 +1,3 @@
-
-
-
-
-//--Problem solving with function
-
-
-
 //-- write a function for a food delivery service that sell "pizza" only.Each pizza price $10, this function takes the food name & quantity as parameter and then return a custom message with order info. This function should be smart enought to handle wrong parameter inputs.
 
 function pizzaPanda (foodName, quantity){
