0f5edd8dd7d6a3a1ee5e9aa44232906e004932cd
devriazul <engr.riazul@gmail.com>
2022-01-30 23:00:31 +0600

    Day 31

--- Day_31/conceptual.js
+++ Day_31/conceptual.js
@@ -20,8 +20,9 @@ var person = {
     name : "Riazul Islam",
     age : 28,
     height : "5'7",
-    hairColor : "Black"
+    hairColor : "Black",
+    card : [1, 2, 3, 4, 5]
 }
 
-console.log(person.name);
+console.log(person.card[2]);
 
