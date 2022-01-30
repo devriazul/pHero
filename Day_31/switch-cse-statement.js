70bd8a1fc46e2a29156df32e4ffd4d4de4ce2371
devriazul <engr.riazul@gmail.com>
2022-01-30 23:20:27 +0600

    Day 31

--- Day_31/switch-cse-statement.js
+++ Day_31/switch-cse-statement.js
@@ -1,13 +1,19 @@
+var age = 40;
+
 switch(age){
     case 40:
         console.log("Too old!");
+        break;
 
     case 25:
         console.log("Young!");
+        break;
     
     case 10:
         console.log("Child");
+        break;
     
     default:
         console.log("I dont know!");
+        break;
 }
