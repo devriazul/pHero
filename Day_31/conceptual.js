b9256a2ac2a3490579513665ea58a16f0380c00b
devriazul <engr.riazul@gmail.com>
2022-01-30 22:14:58 +0600

    Day 31

--- Day_31/conceptual.js
+++ Day_31/conceptual.js
@@ -19,4 +19,4 @@
 // var name = message ("Riazul");
 // console.log(name);
 
-// write a function for foor a food delivery servvice that sell pizza only. this function takes the food name & quantity as parameter and then return a custom message with order info.
+// write a function for foor a food delivery service that sell "pizza" only. this function takes the food name & quantity as parameter and then return a custom message with order info. This function should be smart enought to handle wrong parameter inputs.
