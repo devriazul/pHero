e58ed73d5a014f15ec7cf6b58338c92bfa5a4d0c
devriazul <engr.riazul@gmail.com>
2022-01-30 22:33:32 +0600

    Day 31

---
+++ Day_31/calculator-with-function.js
@@ -0,0 +1,6 @@
+//--make a calculator
+
+// function sumOfNumbers (num1, num2){
+//     var result = num1 + num2;
+//     return result;
+// }
--- Day_31/conceptual.js
+++ Day_31/conceptual.js
@@ -1,9 +1,4 @@
-//--make a calculator
 
-// function sumOfNumbers (num1, num2){
-//     var result = num1 + num2;
-//     return result;
-// }
 
 // console.log(sumOfNumbers(20,9));
 
