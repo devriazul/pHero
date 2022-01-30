56c14ae33d7718a161cf5e22cd3251298837d74d
devriazul <engr.riazul@gmail.com>
2022-01-30 22:35:10 +0600

    Day 31

---
+++ Day_31/problem-solving-2.js
@@ -0,0 +1,13 @@
+//-- write a function for a food delivery service that sell "pizza" only.Each pizza price $10, this function takes the food name & quantity as parameter and then return a custom message with order info. This function should be smart enought to handle wrong parameter inputs.
+
+function pizzaPanda (foodName, quantity){
+    if (foodName.toLowerCase() !== "pizza") {
+        return "Sorry we sell pizza only!";
+    }
+
+    var price = 10 * quantity;
+    var customMessage = "Order Successfull! Order Info : " + foodName + ", Price: "+ price + "$";
+    return customMessage;
+}
+
+console.log(pizzaPanda('Pizza',3));
