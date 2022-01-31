27d5b888231cf2e8f894db41a0dfaeaf49492a4b
devriazul <engr.riazul@gmail.com>
2022-01-31 20:31:59 +0600

    Day 32

--- Day_32/math.js
+++ Day_32/math.js
@@ -1,7 +1,10 @@
 // const myNumber = -5;
 // const output = Math.abs(myNumber);
 
-const myNumber = 2.1;
-const output = Math.ceil(myNumber);
+// const myNumber = 2.1;
+// const output = Math.ceil(myNumber);
+
+const myNumber = 2.7;
+const output = Math.floor(myNumber);
 
 console.log(output);
