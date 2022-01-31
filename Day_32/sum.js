3e0a3466a0c2ade232a60fe1f47dc528bb287e70
devriazul <engr.riazul@gmail.com>
2022-01-31 21:19:31 +0600

    Day 32

--- Day_32/sum.js
+++ Day_32/sum.js
@@ -2,6 +2,8 @@ const numbers = [11, 2, 4, 5, 7];
 
 for (let i = 0; i < numbers.length; i++) {
     const element = numbers[i];
+    var sum = 0;
+    sum = sum + element;
     console.log(element);
     
 }
