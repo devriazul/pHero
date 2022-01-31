e446e844a14d96ba8ad1d3528bcc1c8c62c27b91
devriazul <engr.riazul@gmail.com>
2022-01-31 21:04:49 +0600

    Day 32

--- Day_32/max.js
+++ Day_32/max.js
@@ -1,8 +1,17 @@
 const business = 450;
-const minister = 350;
+const minister = 550;
+const army = 650;
 
-if (business > minister) {
-    console.log("Business man er pola boroloks!");
-}else{
-    console.log("Ministerer pola boroloks");
+// if (business > minister) {
+//     console.log("Business man er pola boroloks!");
+// }else{
+//     console.log("Ministerer pola boroloks");
+// }
+
+if (business > minister && business > army) {
+    console.log("Business best");
+}else if (minister > business && minister > army) {
+    console.log("Minister best");
+}else if (army > business && army > minister) {
+    console.log("Army bigger");
 }
