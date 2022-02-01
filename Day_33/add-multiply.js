6946bf768167c98e32509612a587b2e9a0769688
devriazul <engr.riazul@gmail.com>
2022-02-01 22:36:12 +0600

    Day 33

--- Day_33/add-multiply.js
+++ Day_33/add-multiply.js
@@ -7,3 +7,11 @@ const firstTotal = add(63, 7);
 console.log(firstTotal);
 const secondTotal = add (54, 81);
 console.log(secondTotal);
+
+function multiply (number1, number2){
+    const result = number1 * number2;
+    return result;
+}
+
+const firstResult = multiply(5, 12);
+console.log(firstResult);
