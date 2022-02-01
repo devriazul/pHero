298e3926021db793907e879fb524b347172d9ca4
devriazul <engr.riazul@gmail.com>
2022-02-01 23:46:51 +0600

    Day 33

--- Day_33/divisible.js
+++ Day_33/divisible.js
@@ -1,7 +1,10 @@
 for (let i = 0; i <= 10; i++) {
-    if (i%3 == 0) {
+    if (i % 3 == 0 && i % 5) {
+        console.log('foobar');
+    }else if (i % 3 == 0) {
         console.log('foo');
-    }else if (i % 5 == 0) {
+    }
+    else if (i % 5 == 0) {
         console.log('bar');
     }
     else{
