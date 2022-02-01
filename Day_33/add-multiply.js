8fd765d43615b40233b6958ac7c2661b8e9a3a59
devriazul <engr.riazul@gmail.com>
2022-02-01 22:34:12 +0600

    Day 33

---
+++ Day_33/add-multiply.js
@@ -0,0 +1,9 @@
+function add (num1, num2){
+    const sum = num1 + num2;
+    return sum;
+}
+
+const firstTotal = add(63, 7);
+console.log(firstTotal);
+const secondTotal = add (54, 81);
+console.log(secondTotal);
--- Day_33/reverse.js
+++ Day_33/reverse.js
@@ -1,10 +1,9 @@
 const greetings = "Hello how are you?";
 
 function reverseString (text){
-    let reverse = "";
+    let reverse = ' ';
     for (const letter of text){
-        console.log(letter);
-        reverse = reverse + letter;
+        reverse =  letter + reverse;
     }
 }
 
