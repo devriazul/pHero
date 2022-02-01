9a8df87ce3d97af53075fc84acc6962b9c2ba1ad
devriazul <engr.riazul@gmail.com>
2022-02-01 21:59:26 +0600

    Day 33

--- Day_33/duplicate.js
+++ Day_33/duplicate.js
@@ -1,5 +1,9 @@
 const names = ['abul', 'kabul', 'babul', 'abul', 'kabul', 'babul'];
 
 function removeDuplicate(names){
-    
+    const unique = [];
+    for (let i = 0; i < names.length; i++) {
+        const element = names[i];
+        console.log(element);
+    }
 }
