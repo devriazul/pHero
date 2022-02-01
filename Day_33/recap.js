3c6d670c0066e55e37c8004c94142d483203b23c
devriazul <engr.riazul@gmail.com>
2022-02-01 21:49:31 +0600

    Day 33

--- Day_33/recap.js
+++ Day_33/recap.js
@@ -3,7 +3,21 @@
 function largestElement (numbers){
     let max = numbers [0];
     for (let i = 0; i < numbers.length; i++) {
-        
-        
+        const element = numbers [i];
+        if (element > max) {
+            max = element;
+        }
+
+        }
+}
+
+function largestElement (numbers){
+    let max = numbers [0];
+    for (let i = 0; i < numbers.length; i++) {
+        const element = numbers [i];
+        if (element > max) {
+            max = element;
+        }
+
         }
 }
