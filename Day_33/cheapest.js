bf1249361c3910e410632bc4d754947e7cd694f6
devriazul <engr.riazul@gmail.com>
2022-02-01 23:53:44 +0600

    Day 33

---
+++ Day_33/cheapest.js
@@ -0,0 +1,26 @@
+const phones = [{
+    ram : 12,
+    room : 128,
+    name : "Samsung",
+    price : 112000
+},
+
+{
+    ram : 8,
+    room : 64,
+    name : "Xiomi",
+    price : 21000
+},
+
+{
+    ram : 8,
+    room : 64,
+    name : "Walton",
+    price : 5000
+}
+]
+
+let cheapest = phones [0];
+for (const phone of phones){
+    console.log(phones);
+}
