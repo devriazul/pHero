7b87dce6b346fd0ee8d8d19988fff29e0965b057
devriazul <engr.riazul@gmail.com>
2022-02-02 00:42:13 +0600

    Day 33

--- Day_33/total.js
+++ Day_33/total.js
@@ -1,15 +1,24 @@
-const products = [
-    {name: 'laptop', price: 42000 },
-    {name: 'shirt', price: 2000 },
-    {name: 'watch', price: 4000 },
-    {name: 'phone', price: 24000 },
+const cart = [
+    {name: 'laptop', price: 42000 , quantity: 3 },
+    {name: 'shirt', price: 2000 , quantity: 3 },
+    {name: 'watch', price: 4000 , quantity: 3 },
+    {name: 'phone', price: 24000 , quantity: 3 },
 
 ];
 
-let totalPrice = 0;
+// let totalPrice = 0;
 
-for (const product of products) {
-    totalPrice = totalPrice + product.price;
+// for (const product of products) {
+//     totalPrice = totalPrice + product.price;
     
+// }
+// console.log(totalPrice);
+
+let cartTotal = 0;
+for (const product of cart){
+    // console.log(product);
+    const productTotal = product.price * product.quantity;
+    cartTotal = cartTotal +productTotal;
+
 }
-console.log(totalPrice);
+console.log(cartTotal);
