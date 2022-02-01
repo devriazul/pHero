62e174af756be142412474e84e460aa0c0c9ad6c
devriazul <engr.riazul@gmail.com>
2022-02-01 23:00:00 +0600

    Day 33

--- Day_33/add-multiply.js
+++ Day_33/add-multiply.js
@@ -4,3 +4,19 @@ table = 10cft
 bed = 50cft
 */
 
+function woodCalculator(chairQuantity, tableQuantity, bedQuantity){
+    const perChairWood = 3;
+    const perTableWood = 10;
+    const perBedWood = 50;
+    // wood calculation
+    const chairWoodQuantity = chairQuantity * perChairWood;
+    const tableWoodQuantity = tableQuantity * perTableWood;
+    const bedWoodQuantity = bedQuantity * perBedWood;
+
+    const totalWood = chairWoodQuantity + tableWoodQuantity + bedWoodQuantity;
+    return totalWood;
+}
+
+const firstOption = woodCalculator(1,1,1);
+console.log(firstOption);
+
