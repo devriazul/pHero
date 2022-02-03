33c38a063656201978043ee790b203cdbfca2eed
devriazul <engr.riazul@gmail.com>
2022-02-03 23:32:27 +0600

    Day 35

--- Day_35/Task-05.js
+++ Day_35/Task-05.js
@@ -1,7 +1,7 @@
 var salary = 30000;
 var age = 31;
 
-if (salary == 30000 && age < 30) {
+if (salary == 30000 && age <= 30) {
     console.log("Ready for grooming <3");
 }else{
     console.log("Tor kopale biya nai vai");
