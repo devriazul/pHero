7cd81f6ee32e9a1b40c04fb405abd7b619316f37
devriazul <engr.riazul@gmail.com>
2022-02-03 23:08:00 +0600

    Day 35

--- Day_35/Task-02.js
+++ Day_35/Task-02.js
@@ -1,4 +1,6 @@
 const genderOfPerson = "Male";
 let nameOfPerson = "Riazul Islam";
 console.log(genderOfPerson, nameOfPerson);
+
 nameOfPerson = "Mahfujur Rahman";
+console.log(genderOfPerson, nameOfPerson);
