6aaa1f63e1e00d246072cdb84568a48557d30ec1
devriazul <engr.riazul@gmail.com>
2022-02-04 00:35:15 +0600

    Day 35

--- Day_35/Task-09.js
+++ Day_35/Task-09.js
@@ -1,6 +1,6 @@
 var arr = ["riaz","hridoy","shorif","selim"];
 
-for (let arr = 0; arr < array.length; arr++) {
-    console.log(arr);
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
     
 }
