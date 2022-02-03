873c091c13040364262b29f12a145fd29937bbe2
devriazul <engr.riazul@gmail.com>
2022-02-04 00:11:07 +0600

    Day 35

--- Day_35/Task-07.js
+++ Day_35/Task-07.js
@@ -5,6 +5,14 @@ while (number < 9) {
     number+=2;
 }
 
+//Even Number by While Loop
+var number = 0;
+
+while (number < 9) {
+    console.log(number);
+    number+=2;
+}
+
 // for (let i = 0; i <= 19; i++) {
 //     console.log(i);
 // }
