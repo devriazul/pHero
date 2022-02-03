d4259fca16779b7793e6e5740e2182e10e09eb5a
devriazul <engr.riazul@gmail.com>
2022-02-03 23:29:25 +0600

    Day 35

--- Day_35/Task-04.js
+++ Day_35/Task-04.js
@@ -1,9 +1,10 @@
 var age = 28;
-var job = true;
+var job = false;
+cash = 200000;
 
 if (age == 20) {
     console.log("Your are a teeneger");
-}else if (age < 30 && job == true) {
+}else if (age < 30 && job == true || cash == 200000) {
     console.log("Perfect age for marrage");
 }else if (age > 40) {
     console.log("Old for marrage");
