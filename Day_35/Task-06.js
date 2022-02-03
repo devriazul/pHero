1a4415e67eafb1d7b0631f724b34e7dab006a567
devriazul <engr.riazul@gmail.com>
2022-02-03 23:49:15 +0600

    Day 35

--- Day_35/Task-06.js
+++ Day_35/Task-06.js
@@ -1,10 +1,10 @@
-// var i = 0;
+var i = 0;
 
-// while (i >= 19) {
-//     // console.log(i);
-//     i++;
-// }
-// console.log(i);
+while (i < 19) {
+    console.log(i);
+    i++;
+    console.log(i);
+}
 
 // for (let i = 0; i < 19; i++) {
 //     const element = [i];
