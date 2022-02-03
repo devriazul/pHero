94403725fa5387a95274626fb547b0bcb3a1a232
devriazul <engr.riazul@gmail.com>
2022-02-04 01:10:07 +0600

    Day 35

---
+++ Day_35/Task-11.js
@@ -0,0 +1,9 @@
+var num1 = 4;
+var num2 = 6;
+var num3 = 8;
+
+function multiplication(digit1,digit2,digit3){
+    return digit1*digit2*digit3;
+}
+const final = multiplication (num1,num2,num3);
+console.log(final);
