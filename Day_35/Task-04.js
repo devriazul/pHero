776baa32b084ef1ca0aae810f03daacb5ed95248
devriazul <engr.riazul@gmail.com>
2022-02-03 23:23:22 +0600

    Day 35

--- Day_35/Task-04.js
+++ Day_35/Task-04.js
@@ -4,4 +4,6 @@ if (age < 30) {
     console.log("Perfect age for marrage");
 }else if (age > 40) {
     console.log("Old for marrage");
+}else if (age == 50) {
+    console.log("Your enegy is going to shutdown");
 }
