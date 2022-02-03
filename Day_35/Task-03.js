c50f7bc86b03a092a870bc7116c7af7fc8e9197a
devriazul <engr.riazul@gmail.com>
2022-02-03 23:16:09 +0600

    Day 35

--- Day_35/Task-03.js
+++ Day_35/Task-03.js
@@ -1,10 +1,10 @@
 let num1 = 100;
 let num2 = 10;
 
-let sumAddition = num1+num2;
+let sumAddition = num1 + num2;
 console.log(sumAddition);
 
-let sumAddition = num1+num2;
+let sumAddition = num1 - num2;
 console.log(sumAddition);
 
 let sumAddition = num1+num2;
