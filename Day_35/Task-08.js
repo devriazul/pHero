94d84f56da4d56bb2f52f2a67213f24517233073
devriazul <engr.riazul@gmail.com>
2022-02-04 00:18:55 +0600

    Day 35

--- Day_35/Task-08.js
+++ Day_35/Task-08.js
@@ -2,3 +2,5 @@ var riazFriends = ["selim", "hridoy", "Tahid", "Shorif"];
 console.log(riazFriends.length);
 riazFriends[3] = "Aminur";
 console.log(riazFriends);
+var sumOfArray = riazFriends[1]+riazFriends[2];
+console.log(sumOfArray);
