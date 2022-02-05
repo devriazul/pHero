556f2d7394a466b458ca44e43fc79ebe7c52b4fd
devriazul <engr.riazul@gmail.com>
2022-02-05 23:49:42 +0600

    Day 37

--- Day_37/sum.js
+++ Day_37/sum.js
@@ -1,6 +1,10 @@
-let sum = 0;
-for (let i = 5; i >= 1; i--) {
-    sum = sum + 1;
+// let sum = 0;
+// for (let i = 5; i >= 1; i--) {
+//     sum = sum + 1;
+    
+// }
+// console.log(sum);
+
+function sum(i){
     
 }
-console.log(sum);
