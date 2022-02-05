fbea02b2901304becfd74d7d54d752f1d91a24fc
devriazul <engr.riazul@gmail.com>
2022-02-06 00:22:44 +0600

    Day 37

---
+++ Day_37/beak.js
@@ -0,0 +1,8 @@
+const product = [
+    {name: 'iphone', price: '20000'},
+    {name: 'samsung', price: '10000'},
+    {name: 'lenovo', price: '50000'},
+    {name: 'smartwatch', price: '2000'},
+    {name: 'applewatch', price: '7000'},
+    {name: 'asus', price: '35000'},
+];
