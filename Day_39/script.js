fa30acf698bd33ac19f4f90bd096384e9fc14da5
devriazul <engr.riazul@gmail.com>
2022-02-07 21:34:41 +0600

    Day 39

--- Day_39/script.js
+++ Day_39/script.js
@@ -9,6 +9,8 @@ function makeYellow(){
     document.body.style.backgroundColor = 'yellow';
 }
 
+
+//anonymus function
 const greenButton = document.getElementById('make-green');
 greenButton.onclick = function makeGreen(){
     document.body.style.backgroundColor = 'green';
