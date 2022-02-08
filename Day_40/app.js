3504e3f12a5eb10ba364a94c52910d16439ae2b3
devriazul <engr.riazul@gmail.com>
2022-02-08 21:40:41 +0600

    Day 40

--- Day_40/app.js
+++ Day_40/app.js
@@ -1,7 +1,7 @@
 const allButton = document.getElementsByClassName("button-test");
 const prime = document.getElementById("prime");
 
-console.log(allButton[0]);
+console.log(allButton);
 // console.log(prime);
 
 // for (const button of allButton){
