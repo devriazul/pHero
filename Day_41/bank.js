34b8d5634bf1dc99573290ab555aa3efcc7674ce
devriazul <engr.riazul@gmail.com>
2022-02-09 22:05:22 +0600

    Day 41

--- Day_41/bank.js
+++ Day_41/bank.js
@@ -1,5 +1,8 @@
 document.getElementById('login-submit').addEventListener('click',function(){
     const emailField = document.getElementById('user-email');
     const userEmail = emailField.value;
-    console.log(userEmail);
+    //get password
+
+    const passwordField = document.getElementById('user-password');
+    const userPassword = passwordField.value;
 })
