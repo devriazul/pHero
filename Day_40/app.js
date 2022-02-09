14fd9059ea655bceebdff19969eb3bd6708ba69a
devriazul <engr.riazul@gmail.com>
2022-02-09 20:29:31 +0600

    Day 41

--- Day_40/app.js
+++ Day_40/app.js
@@ -4,9 +4,11 @@ const prime = document.getElementById("prime");
 console.log(allButton);
 // console.log(prime);
 
-// for (const button of allButton){
-//     console.log(button);
-// }
+for (const button of allButton){
+    button.addEventListener("click",function(e){
+        console.log(e.target.parentNode);
+    });
+}
 
 // for (let i = 0; i < allButton.length; i++) {
 //     const element = allButton[i];
