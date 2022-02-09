eefbdeb7d252617edc3a5cfe572409d17d28997d
devriazul <engr.riazul@gmail.com>
2022-02-09 23:19:58 +0600

    Day 41

--- Day_41/banking.js
+++ Day_41/banking.js
@@ -4,4 +4,9 @@ document.getElementById('deposit-button').addEventListener('click', function(){
     const depositInput = document.getElementById('deposit-input');
     const depositAmount = depositInput.value;
     console.log(depositAmount);
-})
+
+    const depositTotal = document.getElementById('deposit-total');
+    depositTotal.innerText = depositAmount;
+});
+
+
