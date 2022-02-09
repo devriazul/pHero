dfa6d0654e6460778137301e62eab731b660a0dd
devriazul <engr.riazul@gmail.com>
2022-02-09 22:12:59 +0600

    Day 41

--- Day_41/bank.js
+++ Day_41/bank.js
@@ -6,7 +6,9 @@ document.getElementById('login-submit').addEventListener('click',function(){
     const passwordField = document.getElementById('user-password');
     const userPassword = passwordField.value;
 
-    if(userEmail == 'sontan@gmail.com' && userPassword == 'secrect'){
+    if(userEmail == 'sontan@gmail.com' && userPassword == 'secret'){
         console.log('Valid User');
+    }else{
+        console.log("False information");
     }
 })
