79f2e96bb4e2a4a5e05ee909ddb9eb6a30c6e3a2
devriazul <engr.riazul@gmail.com>
2022-02-10 00:06:50 +0600

    Day 41

--- Day_41/banking.js
+++ Day_41/banking.js
@@ -5,17 +5,24 @@ document.getElementById('deposit-button').addEventListener('click', function(){
 
     const newDepositText = depositInput.value;
     const newDepositAmount = parseFloat(newDepositText);
-
+// update deposit total
     const depositTotal = document.getElementById('deposit-total');
 
     const previusDepositText = depositTotal.innerText;
     const previusDepositAmount = parseFloat(previusDepositText);
 
     const newDepositTotal = previusDepositAmount + newDepositAmount;
-    
+
     depositTotal.innerText = newDepositTotal;
 
     // clear input field 
+
+// update account balance
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceTotalText = balanceTotal.innerText;
+    const previusBalanceTotal = parseFloat(balanceTotalText);
+    const newBalanceTotal = previusBalanceTotal + newDepositAmount;
+    balanceTotal.innerText = newBalanceTotal;
 
     depositInput.value = '';
 });
