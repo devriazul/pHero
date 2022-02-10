03e61693178d828a5c297becf49543189a25c4da
devriazul <engr.riazul@gmail.com>
2022-02-10 20:43:11 +0600

    Day 42

--- Day_42/bank.js
+++ Day_42/bank.js
@@ -1,3 +1,6 @@
 document.getElementById('deposit-button').addEventListener('click', function(){
+    const depositInput = document.getElementById('deposit-input');
     
+    const depositAmount = depositInput.value;
+    console.log(depositAmount);
 })
