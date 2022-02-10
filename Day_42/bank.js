fe86969e1c1c3029e5efef273998567ba7b2813a
devriazul <engr.riazul@gmail.com>
2022-02-10 21:30:47 +0600

    Day 42

--- Day_42/bank.js
+++ Day_42/bank.js
@@ -14,7 +14,30 @@ document.getElementById('deposit-button').addEventListener('click', function(){
     depositTotal.innerText = previusDepositTotal + depositAmount;
     // console.log(depositTotalText);
 
+    // update balance
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceTotalText = balanceTotal.innerText;
+    const previusBalanceTotal = parseFloat(balanceTotalText);
+    balanceTotal.innerText = previusBalanceTotal + depositAmount;
 
     // clear input field
     depositInput.value = '';
+});
+// Handle withdraw 
+
+document.getElementById('withdraw-button').addEventListener('click', function(){
+   const withdrawInput = document.getElementById('withdraw-input');
+   const withdrawAmountText = withdrawInput.value;
+   const withdrawAmount = parseFloat(withdrawAmountText);
+
+   // update total
+   const withdrawTotal = document.getElementById('withdraw-total');
+   const preWithdrawTotalText = withdrawTotal.innerText;
+   const preWithdrawTotal = parseFloat(preWithdrawTotalText);
+   withdrawTotal.innerText = preWithdrawTotal + withdrawAmount;
+
+   //clear withdraw input
+
+   withdrawInput.value = '';
+
 })
