05031e8c3a33c8a1cb2eb5e646ae064f6d7477e9
devriazul <engr.riazul@gmail.com>
2022-02-10 20:22:58 +0600

    Day 42

---
+++ Day_42/login.js
@@ -0,0 +1,16 @@
+
+// get user email
+    document.getElementById('login-submit').addEventListener('click',function(){
+    const emailField = document.getElementById('user-email');
+    const userEmail = emailField.value;
+
+//get password
+    const passwordField = document.getElementById('user-password');
+    const userPassword = passwordField.value;
+
+// check email & password
+    if(userEmail == 'sontan@gmail.com' && userPassword == 'secret'){
+        window.location.href = 'banking.html';
+    }
+})
+
