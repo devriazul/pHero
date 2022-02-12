f60176d085dc99a27243986ea847d6b15af05614
devriazul <engr.riazul@gmail.com>
2022-02-12 20:47:13 +0600

    Day 43

--- Day_43/app.js
+++ Day_43/app.js
@@ -1,3 +1,6 @@
 document.getElementById('case-plus').addEventListener('click', function(){
-    console.log('ok and + worked');
-})
+    const caseInput = document.getElementById('case-number');
+    const caseNumber = caseInput.value;
+    caseInput.value =parseInt (caseNumber) +1;
+    console.log(caseNumber);
+});
