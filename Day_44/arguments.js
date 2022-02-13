257e56f626304e53567f57c1b3a18aedb60cff5a
devriazul <engr.riazul@gmail.com>
2022-02-13 23:59:08 +0600

    Day 44

--- Day_44/arguments.js
+++ Day_44/arguments.js
@@ -1,7 +1,8 @@
 function addNumber(num1, num2){
-    console.log(arguments[1]);
-    const result = num1+num2;
-    return result;
+    // console.log(arguments[1]);
+    for(const num of arguments){
+        sum = sum + num;
+    }
 }
 const sum = addNumber (23, 13, 100, 200);
 console.log(sum);
