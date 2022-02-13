6649420dc4ad1e79a316a809dea3e6a46c796db5
devriazul <engr.riazul@gmail.com>
2022-02-13 23:26:13 +0600

    Day 44

--- Day_44/array-slice.js
+++ Day_44/array-slice.js
@@ -4,3 +4,8 @@ const numbers = [3,2,19,4,30,2,40,5,67,8,9];
 const numberSliced = numbers.slice(4, 8);
 // console.log(numberSliced);
 // console.log(numbers);
+
+//splice
+const numberSpliced = numbers.splice(4,2);
+console.log(numbers);
+console.log(numberSpliced);
