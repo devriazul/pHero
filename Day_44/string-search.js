693fbd7c1556349f413e68d5054617f81adaa622
devriazul <engr.riazul@gmail.com>
2022-02-13 20:55:13 +0600

    Day 44

---
+++ Day_44/string-search.js
@@ -0,0 +1,11 @@
+const products = [
+    'Dell hardcore i29 200GB Laptop',
+    'Iphone 1TB rom',
+    'yellow laptop with black camera',
+    '1X59 Lenovo commercial Yoga Laptop',
+    'HTC Low Price Phone',
+    'Purple color phone'
+]
+
+const searching = 'laptop';
+// indexOf
