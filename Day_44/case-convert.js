ed8fcc011b1e36b3930dcd581114fec0947ce3f0
devriazul <engr.riazul@gmail.com>
2022-02-13 20:44:13 +0600

    Day 44

--- Day_44/case-convert.js
+++ Day_44/case-convert.js
@@ -2,7 +2,7 @@ const anthem = "Amar sonar bangla ami tomay valobashi";
 const search = 'valo';
 
 const userInput = 'BlackPink';
-const savedUserName = 'blackpink';
+const savedUserName = 'blackPink';
 
 if (userInput.toLowerCase == savedUserName){
     console.log('user exist');
