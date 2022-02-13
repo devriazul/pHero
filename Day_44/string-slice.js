33080f421a1748a1bd28a27737985a94a96998f9
devriazul <engr.riazul@gmail.com>
2022-02-13 22:47:20 +0600

    Day 44

--- Day_44/string-slice.js
+++ Day_44/string-slice.js
@@ -2,3 +2,6 @@ const anthem = "Amar sonar bangla ami tomay valobashi";
 const words = anthem.split(' ');
 const withoutA = anthem.split('a')
 // console.log(withoutA);
+
+const smallSlice = anthem.slice(5, 13);
+console.log(smallSlice);
