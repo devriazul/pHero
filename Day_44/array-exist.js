584b35dae77fb7c7c822b3ee2d2117c82b06c153
devriazul <engr.riazul@gmail.com>
2022-02-13 23:06:14 +0600

    Day 44

--- Day_44/array-exist.js
+++ Day_44/array-exist.js
@@ -1,7 +1,7 @@
 function megaFriend(friends){
     let mega = friends[0];
     for(const friend of friends){
-        if(friend.lenght > mega.lenght){
+        if(friend.length > mega.length){
             mega.friend;
         }
     }return mega;
