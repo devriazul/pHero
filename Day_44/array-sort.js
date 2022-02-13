ccf25d2fef2567f11089d75be2465dac37af77cf
devriazul <engr.riazul@gmail.com>
2022-02-13 23:43:46 +0600

    Day 44

--- Day_44/array-sort.js
+++ Day_44/array-sort.js
@@ -3,7 +3,10 @@ const sortNumber = numbers.sort();
 // console.log(sortNumber);
 const friends = ['asiq', 'kabil', 'josim', 'rubel', 'hasin', 'badsha'];
 // const sortFriends = friends.sort();
-const reverseFriends = friends.reverse();
-console.log(reverseFriends);
+// const reverseFriends = friends.reverse();
+// console.log(reverseFriends);
 
 // reverse sorting
+
+const sortReverseFriend = friends.sort().reverse();
+console.log(sortReverseFriend);
