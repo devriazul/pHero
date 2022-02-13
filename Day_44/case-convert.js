f58a42249c1673ce33eb68fc404fb6f5db3e9c9b
devriazul <engr.riazul@gmail.com>
2022-02-13 20:46:16 +0600

    Day 44

--- Day_44/case-convert.js
+++ Day_44/case-convert.js
@@ -3,6 +3,7 @@ const search = 'valo';
 
 const userInput = 'BlackPink';
 const savedUserName = 'blackpink';
+console.log(userInput.toLowerCase());
 
 if (userInput.toLowerCase() == savedUserName){
     console.log('user exist');
