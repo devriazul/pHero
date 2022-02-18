d4c8956ef6c79602277c5b858d9e95b67adf1ad3
devriazul <engr.riazul@gmail.com>
2022-02-18 18:35:34 +0600

    Day 49

--- Day_49/js/functional.js
+++ Day_49/js/functional.js
@@ -1,3 +1,12 @@
+function getInputValue(fieldId){
+    const inputField = document.getElementById(fieldId);
+    const valueInText = inputField.value;
+    const value = parseFloat(valueInText);
+    inputField.value = '';
+    return value;
+}
+
 document.getElementById('deposit-button').addEventListener('click',function(){
-    console.log('object');
-})
+    const amount = getInputValue('deposit-input');
+    console.log(amount);
+});
