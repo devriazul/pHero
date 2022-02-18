66af6a69486d4b9d8ba6c1d27698f5db20768c0a
devriazul <engr.riazul@gmail.com>
2022-02-18 22:58:32 +0600

    Day 50

--- Day_50_ES6_Intro/dom.js
+++ Day_50_ES6_Intro/dom.js
@@ -3,7 +3,8 @@ document.getElementById('add-border').addEventListener('click', function(){
     container.style.border = '3px solid yellow';
     container.style.padding = '10px';
 });
-document.getElementById('add-bg').addEventListener('click',function(){
-    const container = document.getElementById('friend-container');
-    container.style.backgroundColor = 'gray';
-});
+function addBackgroundColor(){
+const friends = document.getElementsByClassName('friend');
+for(const friend of friends){
+    friend.style.backgroundColor = 'lightblue';
+}};
