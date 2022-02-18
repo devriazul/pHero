c000118c0399b09cfa81a2e8b8d02d1fab1e0d48
devriazul <engr.riazul@gmail.com>
2022-02-18 18:21:36 +0600

    Day 49

---
+++ Day_49/bank.js
@@ -0,0 +1,50 @@
+document.getElementById('deposit-button').addEventListener('click', function(){
+    const depositInput = document.getElementById('deposit-input');
+
+    const depositAmountText = depositInput.value;
+    const depositAmount = parseFloat(depositAmountText);
+    // console.log(depositAmount);
+
+    // get current deposit
+
+    const depositTotal = document.getElementById('deposit-total');
+
+    const depositTotalText = depositTotal.innerText;
+    const previusDepositTotal = parseFloat(depositTotalText);
+    depositTotal.innerText = previusDepositTotal + depositAmount;
+    // console.log(depositTotalText);
+
+    // update balance
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceTotalText = balanceTotal.innerText;
+    const previusBalanceTotal = parseFloat(balanceTotalText);
+    balanceTotal.innerText = previusBalanceTotal + depositAmount;
+
+    // clear input field
+    depositInput.value = '';
+});
+// Handle withdraw 
+
+document.getElementById('withdraw-button').addEventListener('click', function(){
+   const withdrawInput = document.getElementById('withdraw-input');
+   const withdrawAmountText = withdrawInput.value;
+   const withdrawAmount = parseFloat(withdrawAmountText);
+
+   // update total
+   const withdrawTotal = document.getElementById('withdraw-total');
+   const preWithdrawTotalText = withdrawTotal.innerText;
+   const preWithdrawTotal = parseFloat(preWithdrawTotalText);
+   withdrawTotal.innerText = preWithdrawTotal + withdrawAmount;
+
+   // update balance after withdraw
+
+   const balanceTotal = document.getElementById('balance-total');
+   const balanceTotalText = balanceTotal.innerText;
+   const previusBalanceTotal = parseFloat(balanceTotalText);
+   balanceTotal.innerText = previusBalanceTotal - withdrawAmount;
+
+   //clear withdraw input
+
+   withdrawInput.value = '';
+
+})
---
+++ Day_49/banking.js
@@ -0,0 +1,60 @@
+// Handle deposit button
+
+document.getElementById('deposit-button').addEventListener('click', function(){
+    const depositInput = document.getElementById('deposit-input');
+
+    const newDepositText = depositInput.value;
+    const newDepositAmount = parseFloat(newDepositText);
+// update deposit total
+    const depositTotal = document.getElementById('deposit-total');
+
+    const previusDepositText = depositTotal.innerText;
+    const previusDepositAmount = parseFloat(previusDepositText);
+
+    const newDepositTotal = previusDepositAmount + newDepositAmount;
+
+    depositTotal.innerText = newDepositTotal;
+
+    // clear input field 
+
+// update account balance
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceTotalText = balanceTotal.innerText;
+    const previusBalanceTotal = parseFloat(balanceTotalText);
+    const newBalanceTotal = previusBalanceTotal + newDepositAmount;
+    balanceTotal.innerText = newBalanceTotal;
+
+
+// Value 0 making
+    depositInput.value = '';
+});
+
+//handle withdraw
+
+document.getElementById('withdraw-button').addEventListener('click',function(){
+    const withdrawInput = document.getElementById('withdraw-input');
+    const withDrawAmountText = withdrawInput.value;
+    const newWithdrawAmount = parseFloat(withDrawAmountText);
+
+
+    // set withdraw total 
+    const withdrawTotal = document.getElementById('withdraw-total');
+    const previusWithdrawText = withdrawTotal.innerText;
+    const previusWithdrawTotal = parseFloat(previusWithdrawText);
+
+    const newWithdrawTotal = previusWithdrawTotal + newWithdrawAmount;
+
+    withdrawTotal.innerText = newWithdrawTotal
+
+    //update balance
+    const balanceTotal = document.getElementById('balance-total');
+    const previusBalanceText = balanceTotal.innerText;
+    const previusBalanceTotal = parseFloat(previusBalanceText);
+    const newBalanceTotal = previusBalanceTotal - newWithdrawAmount;
+
+    balanceTotal.innerText = newBalanceTotal;
+    // Value 0 making
+    withdrawInput.value = '';
+});
+
+
