8ea20feb8a5744f26ee9e3e993cb0f3fe173f9a7
devriazul <engr.riazul@gmail.com>
2022-02-18 18:55:55 +0600

    Day 49

--- Day_49/js/functional.js
+++ Day_49/js/functional.js
@@ -11,9 +11,24 @@ function updateTotal(fieldId, amount){
     const previusTotal = parseFloat(previusTotalInText);
     const newTotal = previusTotal + amount;
     totalTag.innerText = newTotal;
-
+    return newTotal;
+}
+function updateBalance(){
+    const balanceTag = document.getElementById('balance-total');
+    const balanceInText = balanceTag.innerText;
+    const previusBalance = parseFloat(balanceInText);
+    const newBalance = previusBalance + amount;
+    balanceTag.innerText = newBalance;
 }
 document.getElementById('deposit-button').addEventListener('click',function(){
     const amount = getInputValue('deposit-input');
-    updateTotal('deposit-total', amount)
+    updateTotal('deposit-total', amount);
+    updateBalance(amount);
+});
+
+// handle withdraw
+
+document.getElementById('withdraw-button').addEventListener('click',function(){
+    const amount = getInputValue('withdraw-input');
+    updateTotal('withdraw-total',amount);
 });
