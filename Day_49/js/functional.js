d8d8d5e6acf8642d87e105421493bc8e36a73cea
devriazul <engr.riazul@gmail.com>
2022-02-18 19:01:10 +0600

    Day 49

--- Day_49/js/functional.js
+++ Day_49/js/functional.js
@@ -4,7 +4,7 @@ function getInputValue(fieldId){
     const value = parseFloat(valueInText);
     inputField.value = '';
     return value;
-}
+};
 function updateTotal(fieldId, amount){
     const totalTag = document.getElementById(fieldId);
     const previusTotalInText = totalTag.innerText;
@@ -12,14 +12,14 @@ function updateTotal(fieldId, amount){
     const newTotal = previusTotal + amount;
     totalTag.innerText = newTotal;
     return newTotal;
-}
-function updateBalance(){
+};
+function updateBalance(amount){
     const balanceTag = document.getElementById('balance-total');
     const balanceInText = balanceTag.innerText;
     const previusBalance = parseFloat(balanceInText);
     const newBalance = previusBalance + amount;
     balanceTag.innerText = newBalance;
-}
+};
 document.getElementById('deposit-button').addEventListener('click',function(){
     const amount = getInputValue('deposit-input');
     updateTotal('deposit-total', amount);
@@ -31,4 +31,5 @@ document.getElementById('deposit-button').addEventListener('click',function(){
 document.getElementById('withdraw-button').addEventListener('click',function(){
     const amount = getInputValue('withdraw-input');
     updateTotal('withdraw-total',amount);
+    updateBalance(amount);
 });
