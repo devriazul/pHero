6aa8bd92b447cb41c2bd77de09e3caf92c4fc518
devriazul <engr.riazul@gmail.com>
2022-02-20 22:16:08 +0600

    Conceptual Day

---
+++ Day_50_ES6_Intro/quiz.js
@@ -0,0 +1,7 @@
+// function getSum(a, b=9){return a+b;} console.log(getSum(2, 7));
+// const y = x => x*x; const z = y(5);
+// console.log(z);
+// const myFunc =()=> console.log('Nayok!');
+const cars=['toyota', 'honda','mercedes']; 
+const newCars= [...cars , 'ferrari']; 
+console.log(newCars);
