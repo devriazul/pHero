21d5e58d9f530349b31c3e20e1021e7dab45e678
devriazul <engr.riazul@gmail.com>
2022-02-21 20:40:59 +0600

    Day 53

--- Day_53_API/api.js
+++ Day_53_API/api.js
@@ -2,4 +2,10 @@ function loadData(){
     fetch('https://jsonplaceholder.typicode.com/todos/1')
   .then(response => response.json())
   .then(json => console.log(json))
+};
+
+function loadUsers(){
+    fetch('https://jsonplaceholder.typicode.com/users/1')
+    .then(response => response.json())
+    .then(json => console.log(json))
 }
