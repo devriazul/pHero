6e3bcc82c7e8a3b61fe1124c7ef888cb07c33603
devriazul <engr.riazul@gmail.com>
2022-02-21 20:23:29 +0600

    Day 53

--- Day_53_API/index.js
+++ Day_53_API/index.js
@@ -2,5 +2,17 @@
 //JSON
 const user = { id: 11, name: 'Riaz', Job: 'Developer'}
 const stringified = JSON.stringify(user);
-console.log(stringified);
-console.log(user);
+// console.log(stringified);
+// console.log(user);
+
+const shop = {
+    name: 'Alia Store',
+    address: 'Ranbir Road',
+    Products: ['Laptop','mobile','pepsi'],
+    isExpensive: false
+}
+const shopStringified = JSON.stringify(shop);
+// console.log(shop);
+console.log(shopStringified);
+const converted = JSON.parse(stringified);
+console.log(converted);
