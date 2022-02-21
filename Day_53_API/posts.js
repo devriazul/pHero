baf40b85ee4d549f7ec4cd1984b69e60bc82509c
devriazul <engr.riazul@gmail.com>
2022-02-21 21:21:43 +0600

    Day 53

--- Day_53_API/posts.js
+++ Day_53_API/posts.js
@@ -9,11 +9,12 @@ function displayPosts(posts){
     const postContainer = document.getElementById('posts');
     for(const post of posts){
         const div = document.createElement('div');
+        div.classList.add('post');
         div.innerHTML = `
         <h3>${post.title} </h3>
         <p>${post.body} </p>
         `
         postContainer.appendChild(div);
-    console.log(post);
+    // console.log(post);
     }
 }
