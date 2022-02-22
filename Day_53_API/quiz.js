b1f74fb43878ce4cd29036a8cba099404582df94
devriazul <engr.riazul@gmail.com>
2022-02-22 22:14:29 +0600

    Day 54

---
+++ Day_53_API/quiz.js
@@ -0,0 +1,8 @@
+// const premikas= { name: 'keka ferdousi' , cars: { brand: 'toyota'} };
+
+// const {brand} = premikas.cars
+// console.log(brand);
+
+const info={ data: [ { language:"Javascript", library:"React" } ] };
+
+console.log(info.data[0].library);
