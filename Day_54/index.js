6f4fb75325e3c535133e580b18baee58f2779685
devriazul <engr.riazul@gmail.com>
2022-02-22 22:39:14 +0600

    Day 54

--- Day_54/index.js
+++ Day_54/index.js
@@ -1,5 +1,11 @@
 const loadQuotes = () => {
     fetch('https://api.kanye.rest/')
     .then(res => res.json())
-    .then(data => console.log(data));
+    .then(data => displayQuote(data));
+}
+
+const displayQuote = quote => {
+    // console.log(quote);
+    const quoteElement = document.getElementById('quote');
+    quoteElement.innerText= quote.quote;
 }
