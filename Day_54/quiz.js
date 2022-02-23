3347015f064ebda5117a1e71665f131d36a063cb
devriazul <engr.riazul@gmail.com>
2022-02-23 22:38:58 +0600

    Day 54 completed

---
+++ Day_54/quiz.js
@@ -0,0 +1,15 @@
+// const bondCode = ` I am Fake James bond . My new code is: 00${7+1+2}`;
+// console.log(bondCode);
+// const data = { result: [{ userName: { title: 'Mr.', name: "sakib khan" } }] };
+// console.log(data.result[0].userName.name);
+// const p = [1, 2, 3];
+// const q = p.map(n => Math.pow(n, 3));
+// console.log(q);
+// const path = "images/cat.jpg";
+// const div = document.createElement('div');
+// div.innerHTML = `<img src=${images/cat.jpg} alt = "">`;
+// const info = { name: "", phoneNumber: null };
+// console.log(JSON.stringify(info));
+// const array = { hobbies: ["dancing", "singing", "acting"] };
+// console.log(JSON.stringify(array));
+console.log(JSON.stringify({ eventName: "birthday", date: "2022-02-21" }));
