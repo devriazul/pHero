4a01fd6524730bf0fbf769b7cf43fcc01bad2601
devriazul <engr.riazul@gmail.com>
2022-02-27 21:18:47 +0600

    New Day Begin

---
+++ Day_59/api-checklist.js
@@ -0,0 +1,6 @@
+/*
+1. Object declare (string, boolean, number, object, array, method which will return a property value)
+
+2. Template string. minimum 3 property will be dynamic (nested object property)
+
+*/
