f92210097ec288005379d062b193c08b5bed346c
devriazul <engr.riazul@gmail.com>
2022-02-27 20:56:14 +0600

    New Day Begin

---
+++ Day_58/conceptual.js
@@ -0,0 +1,7 @@
+// const obj ={a:1, b:7, c:3, length:2};
+// console.log(Object.keys(obj).length);
+
+// const obj1= {module: 35, video:2}; const obj2= {module: 35, video:2};
+// console.log(obj1 === obj2);
+
+const getGirlFriend= (name = "chokina")=>"name"; console.log(getGirlFriend()); 
